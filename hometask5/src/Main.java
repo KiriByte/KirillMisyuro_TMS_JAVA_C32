f7f77264d179c85d1f5e2ee7e0c3912b2f44239d
Kirill Misyuro <kmisyuro@gmail.com>
2025-02-15 01:31:35 +0300

    complete task2

--- hometask5/src/Main.java
+++ hometask5/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var task1 = new Task1(rand);
-        task1.start();
+        var task2 = new Task2(rand);
+        task2.start();
 
     }
 
