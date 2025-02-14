ad5884b0fa22d5ac6e58e88dbf46bd6315e55b26
Kirill Misyuro <kmisyuro@gmail.com>
2025-02-15 00:45:40 +0300

    complete task1

---
+++ hometask5/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+public class Main {
+
+    static Random rand = new Random();
+
+    public static void main(String[] args) {
+
+        var task1 = new Task1(rand);
+        task1.start();
+
+    }
+
+}
