8d355287c5a97dcd439c79e61765895d2feadc1a
Kirill Misyuro <kmisyuro@gmail.com>
2025-03-12 18:44:08 +0300

    complete task

--- hometask11/src/main/java/com/kiribyte/hometask11/Main.java
+++ hometask11/src/main/java/com/kiribyte/hometask11/Main.java
@@ -1,7 +1,19 @@
 package com.kiribyte.hometask11;
 
+import com.kiribyte.hometask11.exception.WrongLoginException;
+import com.kiribyte.hometask11.exception.WrongPasswordException;
+import com.kiribyte.hometask11.service.UserRegisterService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        var login = "Username";
+        var password = "Password1111111111111111111111111";
+        var confirmPassword = "Confirm Password";
+
+        try {
+            UserRegisterService.register(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ hometask11/src/main/java/com/kiribyte/hometask11/service/UserRegisterService.java
@@ -0,0 +1,41 @@
+package com.kiribyte.hometask11.service;
+
+
+import com.kiribyte.hometask11.exception.WrongLoginException;
+import com.kiribyte.hometask11.exception.WrongPasswordException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegisterService {
+
+    private static final String passwordPattern = "^(?=.*\\d)(?!.*\\s).{1,19}$";
+    private static final Pattern pattern = Pattern.compile(passwordPattern);
+
+    public static void register(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+
+        if (login == null || login.equals("")) {
+            throw new WrongLoginException("Login is null or empty");
+        }
+        if (login.length() > 20) {
+            throw new WrongLoginException("Login is too long");
+        }
+        if (login.contains(" ")) {
+            throw new WrongLoginException("Login contains spaces");
+        }
+
+
+        if (password == null || password.equals("")) {
+            throw new WrongPasswordException("Password is null or empty");
+        }
+        Matcher matcher = pattern.matcher(password);
+        if (!matcher.matches()) {
+            throw new WrongPasswordException("Password is not correct");
+        }
+        if (!confirmPassword.equals(password)) {
+            throw new WrongPasswordException("Password does not match");
+        }
+
+
+    }
+}
