0b39830bba1d1da517ff5f375c51a69da4457283
Kirill Misyuro <kmisyuro@gmail.com>
2025-04-06 19:18:48 +0300

    add new class for task2

--- hometask14/src/main/java/org/example/Main.java
+++ hometask14/src/main/java/org/example/Main.java
@@ -8,31 +8,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        var order1 = new Order("name1");
-        var order2 = new Order("name2");
-        var order3 = new Order("name3");
+        var task1 = new Task1();
+        var task2 = new Task2();
 
-        Set<Order> orders = new HashSet<>();
-        orders.add(order1);
-        orders.add(order2);
-        orders.add(order3);
-
-        //dublicated
-        var order4 = order1;
-        order4.setName("name4");
-        orders.add(order4);
-
-        //add new
-        var order5 = new Order(order2.getId(), "name5", order2.getStatus());
-        orders.add(order5);
-
-        //ignoring. not adding
-        var order6 = new Order(order3.getId(), "name3", order3.getStatus());
-        orders.add(order6);
-
-        for (var order : orders) {
-            System.out.println(order);
-        }
+        task1.run();
+        task2.run();
 
     }
 }
