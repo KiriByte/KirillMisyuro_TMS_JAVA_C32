93e49ac52beb0dcc3dc6ffa96dd47c4fee6326a2
Kirill Misyuro <kmisyuro@gmail.com>
2025-04-10 10:45:13 +0300

    fix spaces

--- hometask16/src/main/java/org/example/Main.java
+++ hometask16/src/main/java/org/example/Main.java
@@ -12,35 +12,34 @@ import java.util.function.Supplier;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-//
 //        1) Predicate
-
         Predicate<Integer> isEven = i -> i % 2 == 0;
         Predicate<Integer> isOdd = i -> i % 2 == 1;
         Predicate<Integer> isPositive = i -> i > 0;
         Predicate<Integer> isNegative = i -> i < 0;
         Predicate<Integer> isZero = i -> i == 0;
+
 //        2) Consumer
         Consumer<String> printResult = System.out::println;
 
 //        3) Supplier
         Supplier<Integer> readNumberFromConsole = () -> {
             Scanner scanner = new Scanner(System.in);
-            int number = 0;
-            boolean validInput = false;
+            var number = 0;
+            var validInput = false;
             while (!validInput) {
                 try {
                     System.out.print("Please enter an integer: ");
                     number = scanner.nextInt();
-                    validInput = true;  // Если дошли до этой точки, значит ввод правильный
+                    validInput = true;
                 } catch (InputMismatchException e) {
                     System.out.println("Invalid input. Please enter a valid integer.");
                     scanner.nextLine();  // Очистка буфера ввода
                 }
             }
-
             return number;
         };
+
 //        4) Function
         Function<Integer, String> analizer = number -> {
             StringBuilder sb = new StringBuilder();
@@ -53,12 +52,11 @@ public class Main {
             return sb.toString();
         };
 
-        int number = readNumberFromConsole.get();
-        String analized = analizer.apply(number);
+        var number = readNumberFromConsole.get();
+        var analized = analizer.apply(number);
         printResult.accept(analized);
 
         //---------------------------------------------------
-
 
         TreeSet<Worker> workers = new TreeSet<>((worker1, worker2) ->
                 Double.compare(worker1.getSalary(), worker2.getSalary()));
