550d1801dc97151bdde72866d67728e19c956c83
Kirill Misyuro <kmisyuro@gmail.com>
2025-05-03 21:40:01 +0300

    add error

--- hometask22/src/main/java/HomeServlet.java
+++ hometask22/src/main/java/HomeServlet.java
@@ -10,16 +10,27 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.util.Random;
 
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        try (PrintWriter pw = resp.getWriter()) {
-            pw.println(LocalDateTime.now()
-                    .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")));
+        try {
+            var dt = LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+            if (new Random().nextBoolean()) {
+                throw new IOException("Random error!");
+            }
+            try (PrintWriter pw = resp.getWriter()) {
+                pw.println(dt);
+            }
+            //throw new IOException("error");
         } catch (IOException ex) {
-            System.err.println(ex);
+            System.err.println(ex.getMessage());
+            if (!resp.isCommitted()) {
+                resp.sendError(500);
+            }
         }
     }
 }
