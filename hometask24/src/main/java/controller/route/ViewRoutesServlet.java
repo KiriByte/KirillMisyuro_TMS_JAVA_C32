32c13cb5e7bb5e4ae759e569c724e9135e193f43
Kirill Misyuro <kmisyuro@gmail.com>
2025-05-10 21:06:29 +0300

    add votes view

--- hometask24/src/main/java/controller/route/ViewRoutesServlet.java
+++ hometask24/src/main/java/controller/route/ViewRoutesServlet.java
@@ -6,13 +6,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import repository.RouteRepository;
+import repository.VoteRouteRepository;
 import repository.impl.RouteRepositoryImpl;
+import repository.impl.VoteRouteRepositoryImpl;
 
 import java.io.IOException;
 
 @WebServlet("/routes")
 public class ViewRoutesServlet extends HttpServlet {
     private final RouteRepository routeRepository = new RouteRepositoryImpl();
+    private final VoteRouteRepository voteRepository = new VoteRouteRepositoryImpl();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,15 +27,16 @@ public class ViewRoutesServlet extends HttpServlet {
         resp.getWriter().write("<html><body>");
         resp.getWriter().write("<h1>Routes</h1>");
         resp.getWriter().write("<table border='1'>");
-        resp.getWriter().write("<tr><th>ID</th><th>Description</th><th>Length</th></tr>");
+        resp.getWriter().write("<tr><th>ID</th><th>Description</th><th>Length</th><th>Votes</th></tr>");
         for (var route : routes) {
+            int votesCount = voteRepository.getVotesCount(route.getId());
             resp.getWriter().write("<tr>");
             resp.getWriter().write("<td>" + route.getId() + "</td>");
             resp.getWriter().write("<td>" + route.getDescription() + "</td>");
             resp.getWriter().write("<td>" + route.getLengthInMeters() + "</td>");
+            resp.getWriter().write("<td>" + votesCount + "</td>");
             resp.getWriter().write("</tr>");
         }
-
         resp.getWriter().write("</table>");
         resp.getWriter().write("</body></html>");
     }
