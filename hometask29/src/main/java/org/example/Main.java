a74c0a6efca2625b32ee2538c2c6cceeb939369b
Kirill Misyuro <kmisyuro@gmail.com>
2025-06-15 19:18:52 +0300

    complete tesk

--- hometask29/src/main/java/org/example/Main.java
+++ hometask29/src/main/java/org/example/Main.java
@@ -1,30 +1,33 @@
 package org.example;
 
 import org.example.model.ImmortalDragon;
-import org.example.model.Person;
+import org.example.factory.FireWizardAbstractFactory;
+import org.example.factory.IceWizardAbstractFactory;
+import org.example.model.Wizard;
+import org.example.factory.WizardAbstractFactory;
 
 public class Main {
     public static void main(String[] args) {
         ImmortalDragon dragon = ImmortalDragon.getInstance();
-        Person wizard = new Person("Гэндальф") {
-        };
+        WizardAbstractFactory fireFactory = new FireWizardAbstractFactory();
+        WizardAbstractFactory iceFactory = new IceWizardAbstractFactory();
 
-        try {
-            //ждем 1 секунду и пробуем убить мертвого дракона
-            Thread.sleep(1000);
-            dragon.tryKill(wizard);
+        Wizard fireWizard = fireFactory.createWizard("Natsu Dragnil");
+        Wizard iceWizard = iceFactory.createWizard("Gray Fullbuster");
+
+        dragon.addListener(fireWizard);
+        dragon.addListener(iceWizard);
+
+        // Ручная атака для демонстрации
+        System.out.println("----------MANUAL ATTACK-----------");
+        fireWizard.useRandomSpell();
+        dragon.tryKill(fireWizard);
+        iceWizard.useSpell(1);
+        dragon.tryKill(iceWizard);
+        System.out.println("------------------------------------");
 
 
-            // Ждем еще 2.5 секунды (всего 3.5 секунды) (уже должен жить)
-            Thread.sleep(2500);
-            dragon.tryKill(wizard);
-
-            //Пробуем призвать еще дракона
-            dragon = ImmortalDragon.getInstance();
-            dragon.showMurderHistory();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        dragon.showMurderHistory();
 
     }
 }
--- hometask29/src/main/java/org/example/model/ImmortalDragon.java
+++ hometask29/src/main/java/org/example/model/ImmortalDragon.java
@@ -1,18 +1,29 @@
 package org.example.model;
 
+import org.example.observer.DragonRessurectionListener;
+import org.example.observer.DragonRessurectionSubject;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 //Singleton
-public class ImmortalDragon {
+public class ImmortalDragon implements DragonRessurectionSubject {
     private static ImmortalDragon instance;
     private static final Object lock = new Object();
     private final List<String> murderAttempts = new ArrayList<>();
     private boolean isDead = false;
+    private List<DragonRessurectionListener> listeners = new ArrayList<>();
 
     private ImmortalDragon() {
-        System.out.println("«I am the storm that is approaching» — кричит дракон.");
+        System.out.println("«I am the storm that is approaching» — crying dragon!.");
+    }
+
+    public boolean getIsDead() {
+        synchronized (lock) {
+            return isDead;
+        }
     }
 
     public static ImmortalDragon getInstance() {
@@ -20,7 +31,7 @@ public class ImmortalDragon {
             if (instance == null) {
                 instance = new ImmortalDragon();
             } else {
-                System.out.println("Дракон уже был призван в этот мир!");
+                System.out.println("The dragon had already been summoned to this world!");
             }
             return instance;
         }
@@ -29,35 +40,61 @@ public class ImmortalDragon {
     public void tryKill(Person person) {
         synchronized (lock) {
             if (isDead) {
-                System.out.println(person.getName() + " бьёт труп дракона. Бесполезно!");
+                System.out.println(person.getName() + " strikes the dragon's body. It's useless!");
                 return;
             }
 
-            murderAttempts.add(person.getName() + " (" + new Date() + ")");
-            System.out.println(person.getName() + " убивает дракона! Но ненадолго...");
-            isDead = true;
-            resurrect();
+            Random random = new Random();
+            double chance = random.nextDouble();
+
+            if (chance < 0.3) {
+                murderAttempts.add(person.getName() + " (" + new Date() + ")");
+                System.out.println(person.getName() + " kills the dragon! But not for long....");
+                isDead = true;
+                resurrect();
+            } else {
+                System.out.println("It didn't work out... Try again another time.");
+            }
         }
     }
 
     private void resurrect() {
-        new Thread(() -> {
-            try {
-                Thread.sleep(3000);
-                synchronized (lock) {
-                    isDead = false;
-                    System.out.println("«Forsakened, I am awakened!» — кричит дракон. Всего попыток убить его: " + murderAttempts.size());
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }).start();
+        try {
+            Random random = new Random();
+            Thread.sleep(random.nextInt(3000) + 1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        isDead = false;
+        System.out.println("«Forsakened, I am awakened!» — crying dragon. Total kill attempts: " + murderAttempts.size());
+
+        // Уведомляем всех слушателей
+        notifyRessurection();
     }
 
     public void showMurderHistory() {
-        System.out.println("Журнал убийц:");
+        System.out.println("Killers journal:");
         for (String attempt : murderAttempts) {
             System.out.println("- " + attempt);
+        }
+    }
+
+    @Override
+    public void addListener(DragonRessurectionListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void removeListener(DragonRessurectionListener listener) {
+        listeners.remove(listener);
+    }
+
+    @Override
+    public void notifyRessurection() {
+        System.out.println("Notifying all observers about dragon resurrection...");
+        for (DragonRessurectionListener listener : listeners) {
+            listener.onDragonRessurection(this);
         }
     }
 }
