a26e1aa3ad8bd8aa539000057ddfbb14d2429221
Kirill Misyuro <kmisyuro@gmail.com>
2025-07-20 00:40:31 +0300

    complete hometask

---
+++ hometask41/src/test/java/org/example/hometask41/Hometask41ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hometask41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hometask41ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
