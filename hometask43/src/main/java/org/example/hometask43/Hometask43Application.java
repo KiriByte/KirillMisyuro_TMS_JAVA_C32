38b96073672bd9526c35d211a2a3f3f028239506
Kirill Misyuro <kmisyuro@gmail.com>
2025-08-02 23:09:34 +0300

    init project

---
+++ hometask43/src/main/java/org/example/hometask43/Hometask43Application.java
@@ -0,0 +1,13 @@
+package org.example.hometask43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hometask43Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hometask43Application.class, args);
+    }
+
+}
---
+++ hometask43/src/test/java/org/example/hometask43/Hometask43ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hometask43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hometask43ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
